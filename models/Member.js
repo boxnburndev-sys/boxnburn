4daf54ade1e548838f47b154142bf388fbedcc82
boxnburndev-sys <boxnburndev@gmail.com>
2025-09-13 09:50:17 +0200

    Delete models directory

--- models/Member.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const memberSchema = new mongoose.Schema({
-  naam: { type: String, required: true },
-  les: { type: String, required: true },
-  contact: { type: String, required: true }
-}, { timestamps: true });
-
-export default mongoose.model("Member", memberSchema);
