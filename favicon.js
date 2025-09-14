adefdb5474d205cc6b81d385ade2447f3d0246a7
boxnburndev-sys <boxnburndev@gmail.com>
2025-09-14 20:12:38 +0200

    Update favicon.js

--- favicon.js
+++ favicon.js
@@ -1,19 +1,18 @@
-(function setFavicon() {
-  const faviconUrl = "https://i.ibb.co/bj5mBF0t/Box-N-Burn-Algemeen.png";
+(function setCircularFavicon() {
+  const svg = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64">
+      <circle cx="32" cy="32" r="32" fill="white"/>
+      <clipPath id="circleMask">
+        <circle cx="32" cy="32" r="32"/>
+      </clipPath>
+      <image href="https://i.ibb.co/bj5mBF0t/Box-N-Burn-Algemeen.png" x="0" y="0" width="64" height="64" clip-path="url(#circleMask)" preserveAspectRatio="xMidYMid slice"/>
+    </svg>
+  `;
+  const url = "data:image/svg+xml;utf8," + encodeURIComponent(svg);
   document.querySelectorAll('link[rel~="icon"], link[rel="shortcut icon"]').forEach(el => el.remove());
-  const shortcut = document.createElement("link");
-  shortcut.rel = "shortcut icon";
-  shortcut.href = faviconUrl;
-  document.head.appendChild(shortcut);
-  ["16x16","32x32","48x48","96x96"].forEach(size => {
-    const link = document.createElement("link");
-    link.rel = "icon";
-    link.href = faviconUrl;
-    link.sizes = size;
-    document.head.appendChild(link);
-  });
-  const apple = document.createElement("link");
-  apple.rel = "apple-touch-icon";
-  apple.href = faviconUrl;
-  document.head.appendChild(apple);
+  const link = document.createElement("link");
+  link.rel = "icon";
+  link.href = url;
+  link.sizes = "any";
+  document.head.appendChild(link);
 })();
